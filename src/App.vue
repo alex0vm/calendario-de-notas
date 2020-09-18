4e46a53f7f0fb76387650e8adad59c14d63382c1
Alejandro Vega <alejovegam@gmail.com>
2020-09-18 11:41:54 -0500

    calendario base

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,22 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <Calendario/>
+  </v-app>
 </template>
+<script src="https://www.gstatic.com/firebasejs/7.20.0/firebase-app.js"></script>
+<script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+import Calendario from './components/Calendario'; 
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  components: {
+    Calendario,
+  },
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,36 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
+
+import VueTextAreaAutosize from 'vue-textarea-autosize'
+Vue.use(VueTextAreaAutosize);
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCoitDZJH5uUtyLw0S7GZIpuq9aG4WiBYA",
+    authDomain: "calendario-vue-e3371.firebaseapp.com",
+    databaseURL: "https://calendario-vue-e3371.firebaseio.com",
+    projectId: "calendario-vue-e3371",
+    storageBucket: "calendario-vue-e3371.appspot.com",
+    messagingSenderId: "419834151121",
+    appId: "1:419834151121:web:4f70f90415fc40654c05f0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+  export const db = firebase.firestore();
+
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
